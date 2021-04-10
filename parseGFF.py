92684e2a1162d239ba99a691e4d6d9acfce50543
Jahnavi Kodali <jkodali@uark.edu>
2021-04-10 17:06:35 -0500

    Extracted sequences for features in genome

--- parseGFF.py
+++ parseGFF.py
@@ -19,21 +19,24 @@ args = parser.parse_args()
 
 # read in FASTA file
 genome = SeqIO.read(args.fasta, 'fasta')
-print(genome.id)
-print(genome.seq)
 
 # open and read in GFF file
 with open(args.gff, 'r') as gff_in:
-  # create a csv reader object
-  reader = csv.reader(gff_in, delimiter='\t')
+    
+    # create a csv reader object
+    reader = csv.reader(gff_in, delimiter='\t')
+    
+    # loop over all the lines in our reader object (i.e., parsed file)
+    for line in reader:
+        start = int(line[3])
+        end = int(line[4])
+        strand = line[6]
+        feature = line[8]  
 
-  # loop over all the lines in our reader object (i.e., parsed file)
-  for line in reader:
-      start  = line[3]
-      end    = line[4]
-      strand = line[6]
-      #print(start)
-      # print(line[3], line[4])
-
-      # extract the sequence
-      print(len(genome.seq))
+        # extract the sequence
+        if reader.line_num == 1:
+            seq_feature = '>' + genome.id + '\n' + feature
+            seq_feature = seq_feature + '\n' + genome.seq[start-1:start-1]
+        else:
+            seq_feature = genome.seq[start-1:end-1] + '\n' + feature
+        print(seq_feature)
